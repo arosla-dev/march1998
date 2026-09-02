637c3b462ff86b8435c9e1499966da02562c1181
gasmask1 <kolotilkinarosla@gmail.com>
2026-09-02 15:22:40 +0300

    one fix

--- src/dlls/combat.cpp
+++ src/dlls/combat.cpp
@@ -856,7 +856,7 @@ void CGib :: BounceGibTouch ( CBaseEntity *pOther )
 	}
 	else
 	{
-		if ( g_Language != LANGUAGE_GERMAN && m_cBloodDecals > 0 && m_bloodColor != DONT_BLEED )
+		if (CVAR_GET_FLOAT("cl_m98_germanycensorship") != 1 && m_cBloodDecals > 0 && m_bloodColor != DONT_BLEED )
 		{
 			vecSpot = pev->origin + Vector ( 0 , 0 , 8 );//move up a bit, and trace down.
 			UTIL_TraceLine ( vecSpot, vecSpot + Vector ( 0, 0, -24 ),  ignore_monsters, ENT(pev), & tr);
